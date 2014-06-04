23e8775387333042cf1266db051633f43cc0ae8d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-06-04 15:12:13 +0900

    fix examples

--- examples/rmdir-r-co-ex.js
+++ examples/rmdir-r-co-ex.js
@@ -1,10 +1,19 @@
 // require dependencies
+
+'use strict';
+
 var co = require('co');
 try {
-  var rmdirRecursive = require('rmdir-recursive');
-} catch (err) {
   var rmdirRecursive = require('../lib/rmdir-recursive');
+} catch (err) {
+  var rmdirRecursive = require('rmdir-recursive');
 }
+
+var fs = require('fs');
+
+try { fs.mkdirSync('/tmp'); } catch (err) { /* ignore */ }
+try { fs.mkdirSync('/tmp/deep'); } catch (err) { /* ignore */ }
+try { fs.mkdirSync('/tmp/deep/more'); } catch (err) { /* ignore */ }
 
 // co generator
 co(function *() {
--- examples/rmdir-r-sync-ex.js
+++ examples/rmdir-r-sync-ex.js
@@ -1,9 +1,18 @@
 // require dependencies
+
+'use strict';
+
 try {
-  var rmdirRecursiveSync = require('rmdir-recursive').sync;
-} catch (err) {
   var rmdirRecursiveSync = require('../lib/rmdir-recursive').sync;
+} catch (err) {
+  var rmdirRecursiveSync = require('rmdir-recursive').sync;
 }
+
+var fs = require('fs');
+
+try { fs.mkdirSync('/tmp'); } catch (err) { /* ignore */ }
+try { fs.mkdirSync('/tmp/deep'); } catch (err) { /* ignore */ }
+try { fs.mkdirSync('/tmp/deep/more'); } catch (err) { /* ignore */ }
 
 var dir = '/tmp/deep';
 try {
