ec30b0670594e827eb43b82ee73f1be8451e7209
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-07-02 23:09:38 +0900

    use aa, remove co

--- examples/rmdir-r-aa-ex.js
+++ examples/rmdir-r-aa-ex.js
@@ -7,14 +7,14 @@ try {
   var rmdirRecursive = require('rmdir-recursive');
 }
 var fs = require('fs');
-var co = require('co');
+var aa = require('../aa');
 
 try { fs.mkdirSync('/tmp'); } catch (err) { /* ignore */ }
 try { fs.mkdirSync('/tmp/deep'); } catch (err) { /* ignore */ }
 try { fs.mkdirSync('/tmp/deep/more'); } catch (err) { /* ignore */ }
 
-// co generator
-var p = co(function *() {
+// aa generator
+var p = aa(function *() {
   var dir = '/tmp/deep';
   try {
     yield rmdirRecursive(dir);
@@ -28,5 +28,5 @@ if (p && p.then && typeof p.then === 'function') p.then(function () {});
 else if (typeof p === 'function') p();
 else {
   console.log(require('util').inspect(p, {colors: true, depth: null}));
-  throw new Error('what is the value returned from co: ' + typeof p);
+  throw new Error('what is the value returned from aa: ' + typeof p);
 }
